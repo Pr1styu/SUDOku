4a02d06ea36e69c3f43ec7a5b998a4440a9a00f9
Pristyák Dávid <davidpristyak@gmail.com>
2021-11-05 17:02:09 +0100

    Implemented file read, and started parsing the data.

--- NativeComponent/main.cpp
+++ NativeComponent/main.cpp
@@ -24,7 +24,40 @@ std::vector<BYTE> readFile(const char* filename)
     return fileData;
 }
 
-int main(){
-    std::cout << "Hello World!" << std::endl;
-    return 0;
+bool parseCAFF(const std::vector<BYTE>& fileData) {
+    int length = 0;
+    if(fileData[0] != 1){
+        printf("Error first block is not the header");
+        return false;
+    }
+    printf("First block id %i \n",fileData[0]);
+    for(int i=1;i<9;i++){
+        length += fileData[i + 1];
+    }
+
+    printf("The length of the first block is %i \n",length);
+    unsigned char magic[5];
+    for(int i=9;i<13;i++){
+       magic[i-9] = fileData[i];
+    }
+    magic[4] = '\0';
+    printf("%s",magic);
+    if(std::strcmp(reinterpret_cast<const char *>(magic), "CAFF\0") != 0){
+        return false;
+    }
+
+    return true;
+}
+
+
+int main(int argc, char** argv){
+    std::vector<BYTE> caffFile = readFile(R"(C:\Users\DP\CLionProjects\SUDOku\NativeComponent\CAFFTest\1.caff)");
+    bool ret = parseCAFF(caffFile);
+    if(ret){
+        return 0;
+    }
+    else{
+        return 1;
+    }
+
 }
