722a42a70d459d7d8465a27f20abd4ce0a33dcd4
Szilágyi Borbála <szilagyiborbala8@gmail.com>
2021-11-06 02:55:03 +0100

    parser

--- NativeComponent/main.cpp
+++ NativeComponent/main.cpp
@@ -7,7 +7,40 @@
 #include <iostream>
 #include <string>
 #include <algorithm>
+#include <cstring>
+
 typedef unsigned char BYTE;
+
+struct CIFF {
+    uint64_t size;
+    std::string caption;
+    std::vector<std::string> tags;
+    BYTE* pixels;
+    CIFF(uint64_t s, std::string c) : size(s), caption(c) { pixels = new BYTE[size]; }
+};
+
+struct DateTime {
+    uint16_t year;
+    BYTE month;
+    BYTE day;
+    BYTE hour;
+    BYTE minute;
+    DateTime(uint16_t y, BYTE m, BYTE d, BYTE h, BYTE min)
+        : year(y), month(m), day(d), hour(h), minute(min) {}
+};
+
+struct Animation {
+    uint64_t duration;
+    CIFF image;
+    Animation(uint64_t d, CIFF i) : duration(d), image(i) {}
+};
+
+struct CAFF {
+    DateTime creation;
+    std::string  creator;
+    std::vector<Animation> images;
+    CAFF(DateTime ct, std::string c) : creation(ct), creator(c) {}
+};
 
 std::vector<BYTE> readFile(const char* filename)
 {
@@ -27,28 +60,192 @@ std::vector<BYTE> readFile(const char* filename)
 }
 
 int parseCAFF(const std::vector<BYTE> &fileData, const std::string& string) {
-    int length = 0;
-    if(fileData[0] != 1){
-        printf("Error first block is not the header");
+    long read = 0; //the number of bytes already read
+
+    if(fileData[read] != 1) {
+        std::cout << "Error: first block is not the header" << std::endl;
         return 50;
     }
-    printf("First block id %i \n",fileData[0]);
-    for(int i=1;i<9;i++){
-        length += fileData[i + 1];
-    }
+    read++;
 
-    printf("The length of the first block is %i \n",length);
-    unsigned char magic[5];
-    for(int i=9;i<13;i++){
-       magic[i-9] = fileData[i];
-    }
-    magic[4] = '\0';
-    printf("%s",magic);
-    if(std::strcmp(reinterpret_cast<const char *>(magic), "CAFF\0") != 0){
+    union bytes64 {
+        BYTE c[8];
+        uint64_t ll;
+    };
+
+    union bytes16 {
+        BYTE c[2];
+        uint16_t sh;
+    };
+
+    union bytes8 {
+        BYTE c;
+        uint8_t sh;
+    };
+
+    //header block length
+    bytes64 h_length;
+    for(int i = 0; i < 8; i++)
+        h_length.c[i] = fileData[read++];
+    std::cout << "Header length: " << h_length.ll << std::endl;
+
+    //***CAFF HEADER***//
+
+    //magic
+    std::string magic = "";
+    for(int i = 0; i < 4; i++)
+       magic += fileData[read++];
+    if(magic.compare("CAFF") != 0) {
+        std::cout << "Error: not a CAFF file" << std::endl;
         return 51;
     }
 
+    //header size
+    bytes64 h_size;
+    for(int i = 0; i < 8; i++)
+        h_size.c[i] = fileData[read++];
+    std::cout << "Header size: " << h_size.ll << std::endl;
 
+    //number of CIFFs
+    bytes64 ciff_num;
+    for(int i = 0; i < 8; i++)
+        ciff_num.c[i] = fileData[read++];
+    std::cout << "Number of CIFFs: " << ciff_num.ll << std::endl;
+
+    //***CAFF CREDITS***//
+    if(fileData[read] != 2) {
+        std::cout << "Error: second block is not the credits" << std::endl;
+        return 50;
+    }
+    read++;
+
+    //credits block length
+    bytes64 c_length;
+    for(int i = 0; i < 8; i++)
+        c_length.c[i] = fileData[read++];
+    std::cout << "Credits length: " << c_length.ll << std::endl;
+
+    //creation date and time
+    bytes16 year;
+    for(int i = 0; i < 2; i++)
+        year.c[i] = fileData[read++];
+    char month = fileData[read++];
+    char day = fileData[read++];
+    char hour = fileData[read++];
+    char minute = fileData[read++];
+    DateTime creation = DateTime(year.sh, month, day, hour, minute);
+    std::cout << "Creation: " << creation.year << "-" << creation.month << "-" << creation.day
+        << ", " << creation.hour << ":" << creation.minute << std::endl;
+
+    //creator length
+    bytes64 creator_len;
+    for(int i = 0; i < 8; i++)
+        creator_len.c[i] = fileData[read++];
+    std::cout << "Creator length: " << creator_len.ll << std::endl;
+
+    //creator
+    std::string creator = "";
+    for(int i = 0; i < creator_len.ll; i++)
+        creator += fileData[read++];
+    std::cout << "Creator: " << creator << std::endl;
+
+    CAFF caff_file = CAFF(creation, creator);
+
+    //***CAFF ANIMATION***//
+    if(fileData[read] != 3) {
+        std::cout << "Error: third block is not the animation" << std::endl;
+        return 50;
+    }
+    read++;
+
+    //animation block length
+    bytes64 a_length;
+    for(int i = 0; i < 8; i++)
+        a_length.c[i] = fileData[read++];
+    std::cout << "Animation length: " << a_length.ll << std::endl;
+
+    //animation blocks, CIFF parse
+    for(int i = 0; i < ciff_num.ll; i++) {
+        //*duration*//
+        bytes64 duration;
+        for(int j = 0; j < 8; j++)
+            duration.c[j] = fileData[read++];
+        std::cout << "CIFF" << i << " duration: " << duration.ll << std::endl;
+
+        //*CIFF data*//
+
+        //magic
+        std::string ciff_magic = "";
+        for(int j = 0; j < 4; j++)
+            ciff_magic += fileData[read++];
+        if(ciff_magic.compare("CIFF") != 0) {
+            std::cout << "Error: not a CIFF file" << std::endl;
+            return 51;
+        }
+
+        //header size
+        bytes64 ciff_header_size;
+        for(int j = 0; j < 8; j++)
+            ciff_header_size.c[j] = fileData[read++];
+        std::cout << "CIFF" << i << " header size:" << ciff_header_size.ll << std::endl;
+
+        //content size
+        bytes64 ciff_content_size;
+        for(int j = 0; j < 8; j++)
+            ciff_content_size.c[j] = fileData[read++];
+        std::cout << "CIFF" << i << " content size:" << ciff_content_size.ll << std::endl;
+
+        //width
+        bytes64 width;
+        for(int j = 0; j < 8; j++)
+            width.c[j] = fileData[read++];
+        std::cout << "CIFF" << i << " width:" << width.ll << std::endl;
+
+        //height
+        bytes64 height;
+        for(int j = 0; j < 8; j++)
+            height.c[j] = fileData[read++];
+        std::cout << "CIFF" << i << " height:" << height.ll << std::endl;
+
+        if(ciff_content_size.ll != (3*width.ll*height.ll)) {
+            std::cout << "Error: wrong content size" << std::endl;
+            return 51;
+        }
+
+        //caption
+        std::string caption = "";
+        while (fileData[read] != '\n')
+            caption += fileData[read++];
+        std::cout << "CIFF" << i << " caption:" << caption << std::endl;
+        read++; //the new line character
+
+        CIFF ciff_file = CIFF(ciff_content_size.ll, caption);
+
+        //tags
+        int tag_len = ciff_header_size.ll - 36 - caption.size();
+        for(int j = 0; j < tag_len; j++) {
+            std::string tag = "";
+            while (fileData[read] != '\0')
+                tag += fileData[read++];
+            j += (tag.length()+2);
+            read++;
+            ciff_file.tags.push_back(tag);
+            std::cout << tag << std::endl;
+        }
+
+        //pixels
+        for(int j = 0; j < ciff_content_size.ll; j++)
+            ciff_file.pixels[j] = fileData[read++];
+
+        Animation animation = Animation(duration.ll, ciff_file);
+        caff_file.images.push_back(animation);
+    }
+
+    if (read != fileData.size()) {
+        std::cout << "Error: parse bug" << std::endl;
+        std::cout << "Read = " << read << ", Size = " << fileData.size() << std::endl;
+        return 51;
+    }
 
     return 0;
 }
@@ -66,7 +263,7 @@ bool checkFileAvailability(const char * filename){
 
 
 int main(int argc, char* argv []){
-    std::vector<BYTE> caffFile;
+    /*std::vector<BYTE> caffFile;
     std::string out_path;
     if(argc !=2 && argc !=3 && argc != 5){
         std::cout << "The number of arguments given are not 2, 3 or 5" << std::endl;
@@ -166,10 +363,12 @@ int main(int argc, char* argv []){
                 }
             }
         }
-    }
+    }*/
 
-    int ret = parseCAFF(caffFile, out_path);
+    //int ret = parseCAFF(caffFile, out_path);
 
-    return ret;
+    //return ret;
 
+    std::vector<BYTE> caffFile = readFile(R"(C:\Users\Tibor\Desktop\caff_files\1.caff)");
+    return parseCAFF(caffFile, "semmi");
 }
