9b33b64a1c044e3e4e7249c31e836e0b376e3c49
Pristyák Dávid <davidpristyak@gmail.com>
2021-12-02 00:26:58 +0100

    Trying to fix InterruptedException bug

--- caff-service/src/main/java/hu/bme/compsec/sudoku/service/processor/CaffProcessor.java
+++ caff-service/src/main/java/hu/bme/compsec/sudoku/service/processor/CaffProcessor.java
@@ -127,7 +127,6 @@ public final class CaffProcessor {
             final int parseResultCode = parserProcess.exitValue();
             if (parseResultCode == ParseResult.PARSED.getCode()) {
                 log.info("Caff file {} parsed successfully.", savedBaseName);
-                throw new InterruptedException("Test");
             } else {
                 log.error("Caff parser finished with error code: {}", parseResultCode);
                 throw new CaffFileFormatException("Could not parse caff file: {}, code: ", savedBaseName, parseResultCode);
@@ -146,8 +145,6 @@ public final class CaffProcessor {
     private void cleanPreviewAndMetadata() throws IOException {
         Files.deleteIfExists(generatedPreviewPath);
         Files.deleteIfExists(generatedMetaDataPath);
-
-
     }
 
     private String createParserCommand() {
