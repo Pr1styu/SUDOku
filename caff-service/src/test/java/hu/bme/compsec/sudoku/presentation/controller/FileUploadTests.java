05bd79adbeaf398df4d07c9b9be7b1f989b8decc
Orosz Péter <orosz401@gmail.com>
2021-12-01 12:11:41 +0100

    Fix test for findAll

--- caff-service/src/test/java/hu/bme/compsec/sudoku/presentation/controller/FileUploadTests.java
+++ caff-service/src/test/java/hu/bme/compsec/sudoku/presentation/controller/FileUploadTests.java
@@ -3,11 +3,14 @@ package hu.bme.compsec.sudoku.presentation.controller;
 import com.nimbusds.jose.shaded.json.JSONArray;
 import hu.bme.compsec.sudoku.data.domain.CAFFFile;
 import hu.bme.compsec.sudoku.helper.CaffFileHelper;
+import hu.bme.compsec.sudoku.presentation.mapping.CAFFMapper;
 import hu.bme.compsec.sudoku.service.CAFFService;
+import hu.bme.compsec.sudoku.service.CommentService;
 import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
@@ -17,6 +20,7 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.*;
@@ -27,78 +31,89 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
-@AutoConfigureMockMvc
-@SpringBootTest
-@WithMockUser(username="admin", password = "admin", authorities = {"caff:read", "caff:write", "caff:delete"})
+@WebMvcTest(CAFFController.class)
+@WithMockUser(username = "admin", password = "admin", authorities = {"caff:read", "caff:write", "caff:delete"})
 public class FileUploadTests {
 
-	@Autowired
-	private MockMvc mockMvc;
+    @Autowired
+    private MockMvc mockMvc;
 
-	@MockBean
-	private CAFFService caffServiceMock;
+    @MockBean
+    private CAFFService caffServiceMock;
 
-	CaffFileHelper helper = new CaffFileHelper();
+    @MockBean
+    private CAFFMapper caffMapperMock;
 
-	@Test
-	public void shouldListAllFiles() throws Exception {
-		String[] fileNames = new String[] {"1.caff", "3.caff"};
+    @MockBean
+    private CommentService commentServiceMock;
 
-		given(caffServiceMock.getAllCaffFile())
-				.willReturn(helper.loadAllCaffFiles(fileNames));
+    CaffFileHelper helper = new CaffFileHelper();
 
-		mockMvc.perform(get("/caff")
-						.with(user("admin").password("admin")))
-				.andExpect(status().isOk())
-				.andExpect(content().contentType(MediaType.APPLICATION_JSON))
-				.andExpect(content().json(JSONArray.toJSONString(helper.loadAllCaffFiles(fileNames))));
-	}
+    @Test
+    public void shouldListAllFiles() throws Exception {
+        String[] fileNames = new String[]{"1.caff", "2.caff", "3.caff"};
 
-	@Test
-	public void shouldReturnCaffById() throws Exception {
-		final long mockId = 1L;
-		var mockCaffFile = CAFFFile.builder().fileName("test.caff")
-				.id(mockId)
-				.metaData(List.of("test", "meta", "passed"))
-				.preview("Base64EncodedStringPreview".getBytes())
-				.rawBytes("rawBytesOfCassFile".getBytes())
-				.build();
+        given(caffServiceMock.getAllCaffFile())
+                .willReturn(helper.loadAllCaffFiles(fileNames));
 
-		given(caffServiceMock.getCaffFileById(mockId))
-				.willReturn(Optional.of(mockCaffFile));
+        mockMvc.perform(get("/caff")
+                        .with(user("admin").password("admin")))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(content().json(
+                        JSONArray.toJSONString(helper.loadAllCaffFiles(fileNames)
+                                .parallelStream()
+                                .map(caffMapperMock::toPreviewDTO)
+                                .collect(Collectors.toList()))
+                        )
+                );
+    }
 
-		JSONObject caffFilePreviewDto_JSON =
-				new JSONObject().put("id", mockId).put("fileName", "test.caff");
+    @Test
+    public void shouldReturnCaffById() throws Exception {
+        final long mockId = 1L;
+        var mockCaffFile = CAFFFile.builder().fileName("test.caff")
+                .id(mockId)
+                .metaData(List.of("test", "meta", "passed"))
+                .preview("Base64EncodedStringPreview".getBytes())
+                .rawBytes("rawBytesOfCassFile".getBytes())
+                .build();
 
-		mockMvc.perform(get("/caff/"+mockId)
-						.with(user("admin").password("admin")))
-				.andExpect(status().isOk())
-				.andExpect(content().contentType(MediaType.APPLICATION_JSON))
-				.andExpect(content().json(caffFilePreviewDto_JSON.toString()));
+        given(caffServiceMock.getCaffFileById(mockId))
+                .willReturn(Optional.of(mockCaffFile));
 
-		verify(caffServiceMock, times(1)).getCaffFileById(mockId);
-		verifyNoMoreInteractions(caffServiceMock);
-	}
+        JSONObject caffFilePreviewDto_JSON =
+                new JSONObject().put("id", mockId).put("fileName", "test.caff");
 
-	@Test
-	public void shouldIgnoreUploadedFile_OR_BETTER_NAME() throws Exception {
-		MockMultipartFile multipartFile = helper.loadMultipartFile("1.caff");
-		this.mockMvc.perform(multipart("/caff/upload")
-								.file("caffFile", multipartFile.getBytes())
-								.file(new MockMultipartFile("fileName", "testFileName".getBytes())) // TODO: Not sure why this we needed instead of requestAttr
+        mockMvc.perform(get("/caff/" + mockId)
+                        .with(user("admin").password("admin")))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(content().json(caffFilePreviewDto_JSON.toString()));
+
+        verify(caffServiceMock, times(1)).getCaffFileById(mockId);
+        verifyNoMoreInteractions(caffServiceMock);
+    }
+
+    @Test
+    public void shouldIgnoreUploadedFile_OR_BETTER_NAME() throws Exception {
+        MockMultipartFile multipartFile = helper.loadMultipartFile("1.caff");
+        this.mockMvc.perform(multipart("/caff/upload")
+                                .file("caffFile", multipartFile.getBytes())
+                                .file(new MockMultipartFile("fileName", "testFileName".getBytes())) // TODO: Not sure why this we needed instead of requestAttr
 //								.requestAttr("caffFile", multipartFile)
 //								.requestAttr("fileName", "1.caff")
-								.contentType("multipart/form-data")
-								.accept("multipart/form-data")
-				)
-				.andExpect(status().isBadRequest());
+                                .contentType("multipart/form-data")
+                                .accept("multipart/form-data")
+                )
+                .andExpect(status().isBadRequest());
 //				.andExpect(header().string("Location", "/"));
 
-				//.then(this.storageService).should().store(multipartFile);
-	}
+        //.then(this.storageService).should().store(multipartFile);
+    }
 
-	// TODO: Implement this with caffServiceMock return a valid entity and check for 201 etc
-	//public void shouldSaveUploadedFile() throws Exception {
+    // TODO: Implement this with caffServiceMock return a valid entity and check for 201 etc
+    //public void shouldSaveUploadedFile() throws Exception {
 
 
 	/*@SuppressWarnings("unchecked")
