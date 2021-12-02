99b397abb7b625cf48f57982521a655e0ec55595
Nyizsnyik Benjamin <nyizsnyik.benjamin@gmail.com>
2021-12-03 00:00:41 +0100

    [caff-service] disable session and add cors config source

--- caff-service/src/main/java/hu/bme/compsec/sudoku/common/config/security/SecurityConfig.java
+++ caff-service/src/main/java/hu/bme/compsec/sudoku/common/config/security/SecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -18,6 +19,9 @@ import org.springframework.security.oauth2.server.resource.authentication.JwtAut
 import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -31,6 +35,9 @@ public class SecurityConfig {
     SecurityFilterChain securityFilterChainDev(HttpSecurity http) throws Exception {
         http
                 .cors().and()
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
                 .authorizeRequests(authorizeRequests -> authorizeRequests
                         .mvcMatchers("/swagger-ui/**").hasRole(UserRole.ADMIN.name())
                         .requestMatchers(PathRequest.toH2Console()).hasRole(UserRole.ADMIN.name())
@@ -59,6 +66,10 @@ public class SecurityConfig {
     @Profile("prod")
     SecurityFilterChain securityFilterChainJWT(HttpSecurity http) throws Exception {
         http
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .cors().and()
                 .authorizeRequests(authorizeRequests -> authorizeRequests
                         .requestMatchers(PathRequest.toH2Console()).permitAll() // TODO: Turn this off once the db model finalized
                         .mvcMatchers("/swagger-resources/**", "/swagger-ui/**", "/webjars/springfox-swagger-ui/**", "/v2/api-docs**", "/swagger**")
@@ -70,6 +81,13 @@ public class SecurityConfig {
                 .jwtAuthenticationConverter(getJwtAuthenticationConverter());
 
         return http.build();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+        return source;
     }
 
     @Bean
