2ba206695b53a559ecd87a5af16ee5147b3a387b
Nyizsnyik Benjamin <nyizsnyik.benjamin@gmail.com>
2021-12-03 23:36:56 +0100

    [auth-server] fix security hotspot adviced by sonar

--- auth-server/src/main/java/hu/bme/compsec/sudoku/authserver/config/jackson/LongMixin.java
+++ auth-server/src/main/java/hu/bme/compsec/sudoku/authserver/config/jackson/LongMixin.java
@@ -6,11 +6,10 @@ import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.deser.std.NumberDeserializers.LongDeserializer;
 
-@JsonTypeInfo(use = JsonTypeInfo.Id.CLASS)
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME)
 @JsonDeserialize(using = LongDeserializer.class)
 @JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY, getterVisibility = JsonAutoDetect.Visibility.NONE,
         isGetterVisibility = JsonAutoDetect.Visibility.NONE)
 @JsonIgnoreProperties(ignoreUnknown = true)
-public abstract class LongMixin {
-
+public interface LongMixin {
 }
